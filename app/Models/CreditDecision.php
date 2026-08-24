47bdbaeda2db31f4a974e4e1adc5ba65ba75411f
lynelk <86437305+lynelk@users.noreply.github.com>
2026-08-25 00:37:38 +0300

    Link credit decisions to versioned production offers

--- app/Models/CreditDecision.php
+++ app/Models/CreditDecision.php
@@ -20,6 +20,7 @@ class CreditDecision extends Model
         'approved_amount_minor',
         'monthly_income_minor',
         'estimated_obligation_minor',
+        'policy_version',
         'reason_codes',
         'decision_summary',
         'decided_at',
@@ -35,5 +36,15 @@ class CreditDecision extends Model
             'reason_codes' => 'array',
             'decided_at' => 'datetime',
         ];
+    }
+
+    public function application()
+    {
+        return $this->belongsTo(LoanApplication::class, 'loan_application_id');
+    }
+
+    public function offers()
+    {
+        return $this->hasMany(CreditOffer::class);
     }
 }
