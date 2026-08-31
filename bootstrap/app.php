c5d4e7132a0c605f4cb680d8ba6f8b82d98c0a04
lynelk <86437305+lynelk@users.noreply.github.com>
2026-08-31 21:47:56 +0300

    Register experience platform routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/autopilot.php'));
+
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/experience.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
