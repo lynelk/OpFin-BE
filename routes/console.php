21e8586d7b8f4eed50366dbc76ca45f50ef48920
lynelk <86437305+lynelk@users.noreply.github.com>
2026-08-31 21:39:31 +0300

    Schedule platform autopilot checks

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Console\Commands\CheckTransactionStatus;
+use App\Console\Commands\RunPlatformAutopilot;
 use Illuminate\Support\Facades\Schedule;
 
-
 Schedule::command(CheckTransactionStatus::class)->everyMinute();
+Schedule::command(RunPlatformAutopilot::class)->everyFiveMinutes()->withoutOverlapping();
